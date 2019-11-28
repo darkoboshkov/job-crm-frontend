c7b9c48ad0130c3b9e120239bc2cb64d43048662
xinhonglee <xinhong@outlook.com>
2019-11-28 18:14:31 +0200

    update signup page

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import axios from 'axios';
+import '../assets/fonts/Apercu/Apercu.css'
 import '../assets/scss/app.scss'
 
 Vue.config.productionTip = false
---
+++ src/views/auth/SignUp.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        sign up page
+        <div class="form-container">
+            <form class="signup-form">
+                <picture>
+                    <source media="(min-width: 1200px)" srcset="../assets/image/hiway_crm@3x.png">
+                    <source media="(min-width: 768px)" srcset="../assets/image/hiway_crm@2x.png">
+                    <img src="../assets/image/hiway_crm.png" alt="Hiway CRM" style="width:auto;max-width: 100%;">
+                </picture>
+                <h1>Sign Up</h1>
+                <div class="input-group">
+                    <label></label>
+                </div>
+                <div class="input-group"></div>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "SignUp"
+  }
+</script>
+
+<style scoped>
+
+</style>
