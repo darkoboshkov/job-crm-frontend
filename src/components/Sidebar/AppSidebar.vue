ec361349ae3d9e90ca12fb3bbf2ff2e360861256
darkoboshkov <darkoboshkov@protonmail.com>
2019-11-29 17:58:04 +0100

    update sidebar

--- src/components/Sidebar/AppSidebar.vue
+++ src/components/Sidebar/AppSidebar.vue
@@ -1,5 +1,34 @@
 <template>
-  <div class="app-sidebar"></div>
+  <div class="app-sidebar">
+    <div class="app-sidebar__top">
+      <div class="nav-link">
+        <img src="@/assets/image/icon/home.svg" />
+      </div>
+      <div class="nav-link">
+        <img src="@/assets/image/icon/group.svg" />
+      </div>
+      <div class="nav-link">
+        <img src="@/assets/image/icon/bag.svg" />
+      </div>
+      <div class="nav-link">
+        <img src="@/assets/image/icon/hand.svg" />
+      </div>
+      <div class="nav-link">
+        <img src="@/assets/image/icon/calendar.svg" />
+      </div>
+      <div class="nav-link">
+        <img src="@/assets/image/icon/mail.svg" />
+      </div>
+    </div>
+    <div class="app-sidebar__bottom">
+      <div class="nav-link">
+        <img src="@/assets/image/icon/plus.svg" />
+      </div>
+      <div class="nav-link">
+        <img src="@/assets/image/icon/setting.svg" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
