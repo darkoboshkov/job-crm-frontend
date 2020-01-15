c8cad0a202d37bc9c0c52982eb2eaaa984cca6e7
darkoboshkov <darkoboshkov@protonmail.com>
2020-01-16 00:22:08 +0100

    update 404 page

--- src/views/Page404.vue
+++ src/views/Page404.vue
@@ -4,7 +4,6 @@
         <h2>{{ $t('SOMETHING_WRONG') }}</h2>
         <p>Curabitur luctus sit amet nunc et commodo. Phasellus tincidunt, metus eu maximus interdum, dui purus cursus
             elit, nec bibendum tellus erat a libero. Morbi et auctor sapien, nec dignissim mi. </p>
-
         <button class="btn btn-blue large" @click="goToDashBoard">
             <span class="float-left pl-4">{{ $t('BACK_TO_YOUR_DASHBOARD') }}</span>
             <span class="float-right px-4">
@@ -24,7 +23,3 @@
         }
     }
 </script>
-
-<style scoped>
-
-</style>
