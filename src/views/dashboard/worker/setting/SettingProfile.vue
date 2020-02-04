b10165fb4a8859c0ae47d4b93c1273c29d5d67f7
xinhonglee <xinhong@outlook.com>
2020-02-04 16:03:44 +0200

    settings page update with admin role

--- src/views/dashboard/worker/setting/SettingProfile.vue
+++ src/views/dashboard/worker/setting/SettingProfile.vue
@@ -4,28 +4,28 @@
       <div class="form-element">
         <label>{{ $t("FIRST_NAME") }}:</label>
         <b-form-input
-          type="text"
-          required
-          class="custom-input"
-          v-model="model.firstName"
+                type="text"
+                required
+                class="custom-input"
+                v-model="model.firstName"
         />
       </div>
-      <!--<div class="form-element mt-3">-->
-      <!--<label>{{ $t('MIDDLE_NAME') }}:</label>-->
-      <!--<b-form-input-->
-      <!--type="text"-->
-      <!--required-->
-      <!--class="custom-input"-->
-      <!--v-model="model.middleName"-->
-      <!--/>-->
-      <!--</div>-->
+      <div class="form-element mt-3">
+        <label>{{ $t("MIDDLE_NAME") }}:</label>
+        <b-form-input
+                type="text"
+                required
+                class="custom-input"
+                v-model="model.middleName"
+        />
+      </div>
       <div class="form-element mt-3">
         <label>{{ $t("LAST_NAME") }}:</label>
         <b-form-input
-          type="text"
-          required
-          class="custom-input"
-          v-model="model.lastName"
+                type="text"
+                required
+                class="custom-input"
+                v-model="model.lastName"
         />
       </div>
       <div class="form-element mt-3">
@@ -33,63 +33,73 @@
         <div class="gender">
           <b-form-radio v-model="model.gender" name="gender" value="male">{{
             $t("MAN")
-          }}</b-form-radio>
+            }}</b-form-radio>
           <b-form-radio v-model="model.gender" name="gender" value="female">{{
             $t("WOMAN")
-          }}</b-form-radio>
+            }}</b-form-radio>
         </div>
       </div>
       <div class="form-element mt-3">
         <label>{{ $t("DATE_OF_BIRTH") }}:</label>
         <b-form-input
-          type="text"
-          required
-          class="custom-input"
-          v-model="model.birthday"
+                type="text"
+                required
+                class="custom-input"
+                v-model="model.birthday"
         />
       </div>
-      <!--<div class="form-element mt-3">-->
-      <!--<label>{{ $t('BANK_ACCOUNT_NUMBER') }}:</label>-->
-      <!--<b-form-input-->
-      <!--type="text"-->
-      <!--required-->
-      <!--class="custom-input"-->
-      <!--v-model="model.bankNumber"-->
-      <!--/>-->
-      <!--</div>-->
-      <!--<div class="form-element mt-3">-->
-      <!--<label>{{ $t('ADDRESS') }}:</label>-->
-      <!--<b-form-input-->
-      <!--type="text"-->
-      <!--required-->
-      <!--class="custom-input"-->
-      <!--/>-->
-      <!--</div>-->
-      <!--<div class="form-element mt-3">-->
-      <!--<label>{{ $t('POSTAL_CODE') }}:</label>-->
-      <!--<b-form-input-->
-      <!--type="text"-->
-      <!--required-->
-      <!--class="custom-input"-->
-      <!--v-model="model.postalCode"-->
-      <!--/>-->
-      <!--</div>-->
-      <div class="form-element mt-3">
+      <div v-if="role === 'worker'" class="form-element mt-3">
+        <label>{{ $t("BANK_ACCOUNT_NUMBER") }}:</label>
+        <b-form-input
+                type="text"
+                required
+                class="custom-input"
+                v-model="model.bankNumber"
+        />
+      </div>
+      <div v-if="role === 'worker'" class="form-element mt-3">
+        <label>{{ $t("STREET_NAME") }}:</label>
+        <b-form-input
+                type="text"
+                required
+                class="custom-input"
+                v-model="model.street"
+        />
+      </div>
+      <div v-if="role === 'worker'" class="form-element mt-3">
+        <label>{{ $t("HOUSE_NUMBER") }}:</label>
+        <b-form-input
+                type="text"
+                required
+                class="custom-input"
+                v-model="model.houseNumber"
+        />
+      </div>
+      <div v-if="role === 'worker'" class="form-element mt-3">
+        <label>{{ $t("POSTAL_CODE") }}:</label>
+        <b-form-input
+                type="text"
+                required
+                class="custom-input"
+                v-model="model.postalCode"
+        />
+      </div>
+      <div v-if="role === 'worker'" class="form-element mt-3">
         <label>{{ $t("CITY") }}:</label>
         <b-form-input
-          type="text"
-          required
-          class="custom-input"
-          v-model="model.city"
+                type="text"
+                required
+                class="custom-input"
+                v-model="model.city"
         />
       </div>
       <div class="form-element mt-3">
         <label>{{ $t("EMAIL") }}:</label>
         <b-form-input
-          type="text"
-          required
-          class="custom-input"
-          v-model="model.email"
+                type="text"
+                required
+                class="custom-input"
+                v-model="model.email"
         />
       </div>
       <!--<div class="form-element mt-3">-->
@@ -101,17 +111,18 @@
       <!--v-model="model.password"-->
       <!--/>-->
       <!--</div>-->
-      <!--<div class="form-element mt-3">-->
-      <!--<label>{{ $t('PASSPORT') }}:</label>-->
-      <!--<b-form-input-->
-      <!--type="text"-->
-      <!--required-->
-      <!--class="custom-input"-->
-      <!--v-model="model.passport"-->
-      <!--/>-->
-      <!--</div>-->
       <div class="form-element mt-3">
-        <b-button @click="update">{{ $t("update profile") }}</b-button>
+        <label>{{ $t('PASSPORT') }}:</label>
+        <b-form-file
+          class="custom-input"
+          v-model="model.passport"
+          required
+          placeholder="Choose a file or drop it here..."
+          drop-placeholder="Drop file here..."
+        ></b-form-file>
+      </div>
+      <div class="form-element mt-3">
+        <b-button @click="update">{{ $t("UPDATE_PROFILE") }}</b-button>
       </div>
     </div>
     <div class="setting-profile__photo">
@@ -123,44 +134,49 @@
 </template>
 
 <script>
-import profileApi from "@/services/api/profile";
+  import settingsApi from "@/services/api/settings";
 
-export default {
-  name: "SettingProfile",
-  data() {
-    return {
-      model: {
-        role: "",
-        firstName: "",
-        // middleName: "",
-        lastName: "",
-        gender: "male",
-        birthday: "",
-        bankNumber: "",
-        // street: "",
-        // houseNumber: "",
-        postalCode: "",
-        city: "",
-        email: "",
-        password: "",
-        passport: ""
+  export default {
+    name: "SettingProfile",
+    data() {
+      return {
+        model: {
+          firstName: "",
+          middleName: "",
+          lastName: "",
+          gender: "male",
+          birthday: "",
+          bankNumber: "",
+          street: "",
+          houseNumber: "",
+          postalCode: "",
+          city: "",
+          email: ""
+          // password: "",
+          // passport: "",
+        }
+      };
+    },
+    computed: {
+      role() {
+        return this.$store.state.user.role;
       }
-    };
-  },
-  mounted() {
-    profileApi.get().then(res => {
-      console.log("res", res);
-      this.model = res;
-    });
-  },
-  methods: {
-    update() {
-      profileApi.patch(this.model).then(res => {
-        console.log("response", res);
+    },
+    mounted() {
+      settingsApi.get(this.$store.state.user).then(res => {
+        this.model = res;
       });
+    },
+    methods: {
+      update() {
+        settingsApi
+          .patch(Object.assign(this.$store.state.user, this.model))
+          .then(res => {
+            console.log("response", res);
+          });
+      }
     }
-  }
-};
+  };
 </script>
 
 <style scoped></style>
