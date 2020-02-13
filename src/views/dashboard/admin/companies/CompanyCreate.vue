23384322b4f3d99b598ecb2e1b0ed52c15d627b2
xinhonglee <xinhong@outlook.com>
2020-02-14 01:05:03 +0200

    admin company create

--- src/views/dashboard/admin/companies/CompanyCreate.vue
+++ src/views/dashboard/admin/companies/CompanyCreate.vue
@@ -5,154 +5,227 @@
         {{ $t("page_create_company.title") }}
       </h1>
     </div>
+    <div class="company-create__description text-center">
+      <p>
+        {{ $t("page_create_company.description") }}
+      </p>
+    </div>
     <form class="company-create__form mt-4" ref="company-create-form">
       <div class="container">
+        <div class="row align-items-center pb-3">
+          <div class="col-12 col-md-4">
+            <label class="pull-right">{{ $t("page_create_company.form.name") }}:</label>
+          </div>
+          <div class="col-12 col-md-8">
+            <b-form-input
+                    type="text"
+                    name="name"
+                    required
+                    class="custom-input"
+                    v-model="model.name"
+            ></b-form-input>
+          </div>
+        </div>
+        <div class="row align-items-center pb-3">
+          <div class="col-12 col-md-4">
+            <label class="pull-right">{{ $t("page_create_company.form.email") }}:</label>
+          </div>
+          <div class="col-12 col-md-8">
+            <b-form-input
+                    type="email"
+                    name="email"
+                    required
+                    class="custom-input"
+                    v-model="model.email"
+            ></b-form-input>
+          </div>
+        </div>
+        <div class="row align-items-center pb-3">
+          <div class="col-12 col-md-4">
+            <label class="pull-right">{{ $t("page_create_company.form.kvk") }}:</label>
+          </div>
+          <div class="col-12 col-md-8">
+            <b-form-input
+                    type="number"
+                    name="kvk"
+                    required
+                    class="custom-input"
+                    v-model="model.kvkNumber"
+            ></b-form-input>
+          </div>
+        </div>
+        <div class="row align-items-center pb-3">
+          <div class="col-12 col-md-4">
+            <label class="pull-right">{{ $t("page_create_company.form.limit_credit_safe") }}:</label>
+          </div>
+          <div class="col-12 col-md-8">
+            <b-form-input
+                    type="number"
+                    name="limit_credit_safe"
+                    required
+                    class="custom-input"
+                    v-model="model.limitCreditSafe"
+            ></b-form-input>
+          </div>
+        </div>
+        <div class="row align-items-center pb-3">
+          <div class="col-12 col-md-4">
+            <label class="pull-right">{{ $t("page_create_company.form.vat_shifted") }}:</label>
+          </div>
+          <div class="col-12 col-md-8">
+            <div class="row align-items-center">
+              <div class="col-12 col-md-4">
+                <b-form-group class="mb-2">
+                  <b-form-radio-group
+                    id="radio-group-1"
+                    v-model="model.vatShiftedEnabled"
+                    name="vat_shifted"
+                    style="outline: none;"
+                  >
+                    <b-form-radio :value="false">No</b-form-radio>
+                    <b-form-radio :value="true">Yes</b-form-radio>
+                  </b-form-radio-group>
+                </b-form-group>
+              </div>
+              <div class="col-12 col-md-8">
+                <div class="row align-items-center">
+                  <div class="col-12 col-md-4">
+                    <label class="pull-right">Number:</label>
+                  </div>
+                  <div class="col-12 col-md-8">
+                    <b-form-input
+                            type="text"
+                            name="vat_shifted"
+                            :disabled="!model.vatShiftedEnabled"
+                            class="custom-input"
+                            v-model="model.vatShifted"
+                    ></b-form-input>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row align-items-center pb-3">
+          <div class="col-12 col-md-4">
+            <label class="pull-right">{{ $t("page_create_company.form.g_account") }}:</label>
+          </div>
+          <div class="col-12 col-md-8">
+            <div class="row align-items-center">
+              <div class="col-12 col-md-4">
+                <b-form-group class="mb-2">
+                  <b-form-radio-group
+                          id="radio-group-2"
+                          v-model="model.gAccountEnabled"
+                          name="g_account"
+                          style="outline: none;"
+                  >
+                    <b-form-radio :value="false">No</b-form-radio>
+                    <b-form-radio :value="true">Yes</b-form-radio>
+                  </b-form-radio-group>
+                </b-form-group>
+              </div>
+              <div class="col-12 col-md-8">
+                <div class="row align-items-center">
+                  <div class="col-12 col-md-4">
+                    <label class="pull-right">Percentage:</label>
+                  </div>
+                  <div class="col-12 col-md-8">
+                    <b-form-input
+                            type="number"
+                            name="vat_shifted"
+                            :disabled="!model.gAccountEnabled"
+                            class="custom-input"
+                            v-model="model.gAccount"
+                    ></b-form-input>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row align-items-center pb-3">
+          <div class="col-12 col-md-4">
+            <label class="pull-right">{{ $t("page_create_company.form.terms_of_payment") }}:</label>
+          </div>
+          <div class="col-12 col-md-8">
+            <b-form-group class="mb-2">
+              <b-form-radio-group
+                      id="radio-group-3"
+                      v-model="model.termOfPayment"
+                      :options="termsOfPayment"
+                      name="term_of_payment"
+                      style="outline: none;"
+              ></b-form-radio-group>
+            </b-form-group>
+          </div>
+        </div>
+        <div class="row align-items-center pb-3">
+          <div class="col-12 col-md-4">
+            <label class="pull-right">{{ $t("page_create_company.form.automatic_collection") }}:</label>
+          </div>
+          <div class="col-12 col-md-8">
+            <b-form-group class="mb-2">
+              <b-form-radio-group
+                      id="radio-group-4"
+                      v-model="model.automaticCollection"
+                      name="automatic_collection"
+                      style="outline: none;"
+              >
+                <b-form-radio :value="false">No</b-form-radio>
+                <b-form-radio :value="true">Yes</b-form-radio>
+              </b-form-radio-group>
+            </b-form-group>
+          </div>
+        </div>
+        <div class="row align-items-center pb-3">
+          <div class="col-12 col-md-4">
+            <label class="pull-right">{{ $t("page_create_company.form.charge_travel_expenses") }}:</label>
+          </div>
+          <div class="col-12 col-md-8">
+            <b-form-group class="mb-2">
+              <b-form-radio-group
+                      id="radio-group-5"
+                      v-model="model.chargeTravelExpenses"
+                      name="charge_travel_expenses"
+                      style="outline: none;"
+              >
+                <b-form-radio :value="false">No</b-form-radio>
+                <b-form-radio :value="true">Yes</b-form-radio>
+              </b-form-radio-group>
+            </b-form-group>
+          </div>
+        </div>
+        <div class="row align-items-center pb-3">
+          <div class="col-12 col-md-4">
+            <label class="pull-right">{{ $t("page_create_company.form.charge_other_expenses") }}:</label>
+          </div>
+          <div class="col-12 col-md-8">
+            <b-form-group class="mb-2">
+              <b-form-radio-group
+                      id="radio-group-6"
+                      v-model="model.chargeOtherExpenses"
+                      name="charge_other_expenses"
+                      style="outline: none;"
+              >
+                <b-form-radio :value="false">No</b-form-radio>
+                <b-form-radio :value="true">Yes</b-form-radio>
+              </b-form-radio-group>
+            </b-form-group>
+          </div>
+        </div>
         <div class="row">
-          <div class="col-12 col-md-6">
-            <div class="form-element">
-              <label>{{ $t("page_create_company.form.name") }}:</label>
-              <b-form-input
-                type="text"
-                name="name"
-                required
-                class="custom-input"
-                v-model="model.name"
-              />
-            </div>
-          </div>
-          <div class="col-12 col-md-6">
-            <div class="form-element">
-              <label>{{ $t("page_create_company.form.email") }}:</label>
-              <b-form-input
-                type="email"
-                name="email"
-                required
-                class="custom-input"
-                v-model="model.email"
-              ></b-form-input>
-            </div>
-          </div>
-          <div class="col-12 col-md-6">
-            <div class="form-element">
-              <label>{{ $t("page_create_company.form.phone") }}:</label>
-              <b-form-input
-                type="number"
-                name="phone"
-                class="custom-input"
-                v-model="model.phoneNumber"
-              ></b-form-input>
-            </div>
-          </div>
-          <div class="col-12 col-md-6">
-            <div class="form-element">
-              <label>{{ $t("page_create_company.form.kvk") }}:</label>
-              <b-form-input
-                type="number"
-                name="kvk"
-                required
-                class="custom-input"
-                v-model="model.kvkNumber"
-              ></b-form-input>
-            </div>
-          </div>
-          <div class="col-12 col-md-6">
-            <div class="form-element">
-              <label
-                >{{ $t("page_create_company.form.limit_credit_safe") }}:</label
-              >
-              <b-form-input
-                type="number"
-                required
-                class="custom-input"
-                v-model="model.limitCreditSafe"
-              ></b-form-input>
-            </div>
-          </div>
-          <div class="col-12 col-md-6">
-            <div class="form-element">
-              <label>{{ $t("page_create_company.form.vat_shifted") }}:</label>
-              <b-form-input
-                type="text"
-                name="vat_shifted"
-                required
-                class="custom-input"
-                v-model="model.vatShifted"
-              ></b-form-input>
-            </div>
-          </div>
-          <div class="col-12 col-md-6">
-            <div class="form-element">
-              <label>{{ $t("page_create_company.form.g_account") }}:</label>
-              <b-form-input
-                type="text"
-                name="g_account"
-                class="custom-input"
-                v-model="model.gAccount"
-              ></b-form-input>
-            </div>
-          </div>
-          <div class="col-12 col-md-6">
-            <div class="form-element">
-              <label
-                >{{ $t("page_create_company.form.terms_of_payment") }}:</label
-              >
-              <b-form-select
-                name="term_of_payment"
-                required
-                :options="['7 days', '14 days', '30 days']"
-                class="custom-input"
-                v-model="model.termOfPayment"
-              ></b-form-select>
-            </div>
-          </div>
-          <div class="col-12 col-md-6">
-            <div class="form-element">
-              <b-form-checkbox
-                type="text"
-                name="automatic_collection"
-                required
-                class="custom-input"
-                v-model="model.automaticCollection"
-              >
-                {{ $t("page_create_company.form.automatic_collection") }}
-              </b-form-checkbox>
-            </div>
-          </div>
-          <div class="col-12 col-md-6">
-            <div class="form-element">
-              <b-form-checkbox
-                name="charge_travel_expenses"
-                required
-                class="custom-input"
-                v-model="model.chargeTravelExpenses"
-              >
-                {{ $t("page_create_company.form.charge_travel_expenses") }}
-              </b-form-checkbox>
-            </div>
-          </div>
-          <div class="col-12 col-md-6">
-            <div class="form-element">
-              <b-form-checkbox
-                name="charge_other_expenses"
-                required
-                class="custom-input"
-                v-model="model.chargeOtherExpenses"
-              >
-                {{ $t("page_create_company.form.charge_other_expenses") }}
-              </b-form-checkbox>
-            </div>
-          </div>
-          <div class="col-12 col-md-6">
-            <div class="form-element">
-              <b-form-checkbox
-                v-model="model.active"
-                class="custom-input"
-                name="is_active"
-              >
-                {{ $t("page_create_company.form.is_active") }}
-              </b-form-checkbox>
-            </div>
-          </div>
+          <!--<div class="col-12 col-md-6">-->
+            <!--<div class="form-element">-->
+              <!--<b-form-checkbox-->
+                <!--v-model="model.active"-->
+                <!--class="custom-input"-->
+                <!--name="is_active"-->
+              <!--&gt;-->
+                <!--{{ $t("page_create_company.form.is_active") }}-->
+              <!--</b-form-checkbox>-->
+            <!--</div>-->
+          <!--</div>-->
           <div class="col-12 col-md-6">
             <!--<div class="form-element">-->
             <!--<label>{{ $t("COMPANY_MEMBERS") }}:</label>-->
@@ -182,15 +255,35 @@
       modal-class="modal-alert"
     >
       <div class="text-center">
-        <img class="success-image" src="@/assets/image/icon/success.svg" />
+        <img class="success-image" src="@/assets/image/icon/alert.svg" />
         <p class="alert-title color-blue">
-          {{ $t("page_create_company.modal.company_create.title") }}
+          {{ $t("page_create_company.modal.create_error.title") }}
         </p>
         <p class="alert-sub-title">
-          {{ $t("page_create_company.modal.company_create.sub_title") }}
+          {{ error }}
         </p>
         <button class="btn btn-blue" @click="$refs['modal-alert'].hide()">
-          {{ $t("page_create_company.modal.company_create.continue") }}
+          {{ $t("page_create_company.modal.create_error.continue") }}
+        </button>
+      </div>
+    </b-modal>
+    <b-modal
+            ref="modal-success"
+            :hide-footer="true"
+            :hide-header="true"
+            centered
+            modal-class="modal-success"
+    >
+      <div class="text-center">
+        <img class="success-image" src="@/assets/image/icon/success.svg" />
+        <p class="alert-title color-blue">
+          {{ $t("page_create_company.modal.create_success.title") }}
+        </p>
+        <p class="alert-sub-title">
+          {{ $t("page_create_company.modal.create_success.sub_title") }}
+        </p>
+        <button class="btn btn-blue" @click="$refs['modal-success'].hide()">
+          {{ $t("page_create_company.modal.create_success.continue") }}
         </button>
       </div>
     </b-modal>
@@ -199,6 +292,8 @@
 
 <script>
 import companyApi from "../../../../services/api/companies.js";
+import constantsApi from "../../../../services/api/constants.js";
+import errorReader from "@/helpers/ErrorReader";
 
 export default {
   name: "CompanyCreate",
@@ -210,35 +305,61 @@ export default {
         phoneNumber: "",
         kvkNumber: "",
         limitCreditSafe: "",
+        vatShiftedEnabled: false,
         vatShifted: "",
+        gAccountEnabled: false,
         gAccount: "",
         termOfPayment: "",
-        automaticCollection: "",
-        chargeTravelExpenses: "",
-        chargeOtherExpenses: "",
+        automaticCollection: true,
+        chargeTravelExpenses: true,
+        chargeOtherExpenses: true,
         active: false,
         members: []
-      }
+      },
+      termsOfPayment: [],
+      error: ''
     };
   },
   methods: {
+    getTermsOfPayment() {
+      return constantsApi.getAll()
+        .then((res) => {
+          this.termsOfPayment = res.termsOfPayments;
+          this.model.termOfPayment = this.termsOfPayment[0];
+        });
+    },
     create() {
-      // companyApi.create(this.model);
-      console.log("company create");
-      companyApi.create(this.model).then(res => {
-        this.$refs["modal-alert"].show();
-        console.log("res", res);
-      });
+      if (!this.vatShiftedEnabled) {
+        delete this.model.vatShifted;
+      }
+      if (!this.gAccountEnabled) {
+        delete this.model.gAccount;
+      }
+
+      companyApi.create(this.model)
+        .then(res => {
+          this.$refs["modal-success"].show();
+        })
+        .catch(err => {
+          // let read = errorReader(err);
+          // this.error = read.param + ' is ' + read.msg.toLowerCase();
+
+          this.error = err.response.data?.errors?.msg;
+
+          this.$refs["modal-alert"].show();
+        });
+    },
+    catchSubmitCreate(e) {
+      e.preventDefault();
+      this.create();
     }
   },
   mounted() {
-    this.$refs["company-create-form"].addEventListener("submit", e => {
-      e.preventDefault();
-      this.create();
-    });
+    this.getTermsOfPayment();
+    this.$refs["company-create-form"].addEventListener("submit", this.catchSubmitCreate.bind(this));
   },
   beforeDestroy() {
-    this.$refs["company-create-form"].removeEventListener("submit");
+    this.$refs["company-create-form"].removeEventListener("submit", this.catchSubmitCreate);
   }
 };
 </script>
