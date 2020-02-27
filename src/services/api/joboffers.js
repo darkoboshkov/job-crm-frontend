199a4fa0e38af77d4a57db3f249641d416107a34
xinhonglee <xinhong@outlook.com>
2020-02-27 18:35:46 +0200

    remove comments

--- src/services/api/joboffers.js
+++ src/services/api/joboffers.js
@@ -1,14 +1,6 @@
 import request from "../request";
 
 export default {
-  // getAll(params, resolve, reject = null) {
-  //   return request("get", `/jobs`, params, resolve, reject);
-  // },
-  // getCompanyJobs(params, resolve, reject = null) {
-  //   const companyId = params.companyId;
-  //   delete params.companyId;
-  //   return request("get", `/jobs/${companyId}/all`, params, resolve, reject);
-  // },
   get(params, resolve, reject = null) {
     return request(
       "get",
