cea2eb65507d2fd2cde2b2e2093b9d7ab55df510
darkoboshkov <darkoboshkov@protonmail.com>
2020-03-26 11:56:34 +0100

    Add language compare

--- src/views/Language.vue
+++ src/views/Language.vue
@@ -5,10 +5,42 @@
 		<div class="container-fluid">
 			<b-row>
 				<b-col md="6">
-					<v-jsoneditor v-model="EnglishJson" height="calc(100vh - 200px)"/>
+					<vue-json-editor v-model="EnglishJson" height="calc(100vh - 200px)"/>
 				</b-col>
 				<b-col md="6">
-					<v-jsoneditor v-model="DutchJson" height="calc(100vh - 200px)"/>
+					<vue-json-editor v-model="DutchJson" height="calc(100vh - 200px)"/>
+				</b-col>
+			</b-row>
+			<b-row class="mt-5">
+				<b-col md="6">
+					<p class="text-center color-gray">Old Language</p>
+					<vue-json-editor v-model="oldEnglishData" height="200px"/>
+				</b-col>
+				<b-col md="6">
+					<p class="text-center color-gray">Old Language</p>
+					<vue-json-editor v-model="oldDutchData" height="200px"/>
+				</b-col>
+			</b-row>
+			<b-row class="mt-5">
+				<b-col md="6">
+					<p class="text-center color-gray">Change Status</p>
+					<div class="change-box">
+						<vue-json-compare
+								v-if="Object.keys(oldEnglishData).length > 0"
+								:oldData="EnglishJson"
+								:newData="oldEnglishData"
+						/>
+					</div>
+				</b-col>
+				<b-col md="6">
+					<p class="text-center color-gray">Change Status</p>
+					<div class="change-box">
+						<vue-json-compare
+								v-if="Object.keys(oldDutchData).length > 0"
+								:oldData="DutchJson"
+								:newData="oldDutchData"
+						/>
+					</div>
 				</b-col>
 			</b-row>
 		</div>
@@ -16,19 +48,23 @@
 </template>
 
 <script>
-import VJsoneditor from 'v-jsoneditor'
-import EnglishJson from '@/plugins/i18n/languages/English'
-import DutchJson from '@/plugins/i18n/languages/Dutch'
+import vueJsonEditor from 'v-jsoneditor';
+import vueJsonCompare from 'vue-json-compare';
+import EnglishJson from '@/plugins/i18n/languages/English';
+import DutchJson from '@/plugins/i18n/languages/Dutch';
 
   export default {
     name: "Language",
     components: {
-      VJsoneditor
+      vueJsonEditor,
+      vueJsonCompare
     },
 		data() {
       return {
         EnglishJson,
-        DutchJson
+        DutchJson,
+				oldEnglishData: {},
+				oldDutchData: {}
 			}
 		}
   }
