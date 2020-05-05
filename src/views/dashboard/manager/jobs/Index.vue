96035c2c7a30d4cfdebd3edd3676e202d8bb80d5
darkoboshkov <darkoboshkov@protonmail.com>
2020-05-05 12:11:43 +0200

    fix: Remove start matching

--- src/views/dashboard/manager/jobs/Index.vue
+++ src/views/dashboard/manager/jobs/Index.vue
@@ -68,12 +68,12 @@
               <b-dropdown-item @click="goToJob(props)">
                 {{ $t("page_jobs.table.view_job") }}
               </b-dropdown-item>
-              <b-dropdown-item
-                v-if="props.row.status === 'active'"
-                @click="goToMatching(props)"
-              >
-                {{ $t("page_jobs.table.start_matching") }}
-              </b-dropdown-item>
+<!--              <b-dropdown-item-->
+<!--                v-if="props.row.status === 'active'"-->
+<!--                @click="goToMatching(props)"-->
+<!--              >-->
+<!--                {{ $t("page_jobs.table.start_matching") }}-->
+<!--              </b-dropdown-item>-->
             </b-dropdown>
             <button
               class="btn btn-transparent"
